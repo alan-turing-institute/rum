6ca90709646b94a23ddb8bad1253439e9c91060b
James Geddes <jgeddes@turing.ac.uk>
2025-10-20 13:29:00 +0100

    Unify Ada and Lovelace

--- rum-config.rkt
+++ rum-config.rkt
@@ -23,9 +23,9 @@
 (define RUM-ROOMS
   (list
    (office-room "Margaret Hamilton"    "margarethamilton@turing.ac.uk"    "1st floor" 12)
-   (office-room "Augusta Ada"          "augusta@turing.ac.uk"             "1st floor" 12)
-   (office-room "Lovelace"             "lovelace@turing.ac.uk"            "1st floor" 12)
    (office-room "Ursula Franklin"      "ursulafranklin@turing.ac.uk"      "1st floor" 6)
+   (office-room "Ada Lovelace"         "adalovelace@turing.ac.uk"         "1st floor" 12)
+;   (office-room "Lovelace"             "lovelace@turing.ac.uk"            "1st floor" 12)
 
    (office-room "Joan Clarke"          "joanclarke@turing.ac.uk"          "1st floor" 5)
    (office-room "Marian Rejewski"      "marianrejewski@turing.ac.uk"      "1st floor" 5)
@@ -41,10 +41,25 @@
    (office-room "Isaac Asimov"         "isaacasimov@turing.ac.uk"         "2nd floor" 4)
    (office-room "Helen Sharman"        "helensharman@turing.ac.uk"        "2nd floor" 4)
 
-   (office-room "ACE"                  "ace@turing.ac.uk"                 "4th floor" 4)
-   (office-room "Banburismus"          "banburismus@turing.ac.uk"         "4th floor" 4)
-   (office-room "Delilah"              "delilah@turing.ac.uk"             "4th floor" 6)
-   (office-room "Turingery"            "turingery@turing.ac.uk"           "4th floor" 6)))
+   ;; (office-room "ACE"                  "ace@turing.ac.uk"                 "4th floor" 4)
+   ;; (office-room "Banburismus"          "banburismus@turing.ac.uk"         "4th floor" 4)
+   ;; (office-room "Delilah"              "delilah@turing.ac.uk"             "4th floor" 6)
+   ;; (office-room "Turingery"            "turingery@turing.ac.uk"           "4th floor" 6)
+
+   (office-room "Arc"                  "arc-pod@turing.ac.uk"             "Pod" 2)
+   (office-room "Beam"                 "beam-pod@turing.ac.uk"            "Pod" 2)
+   (office-room "Core"                 "core-pod@turing.ac.uk"            "Pod" 2)
+   (office-room "Echo"                 "echo-pod@turing.ac.uk"            "Pod" 2)
+   (office-room "Halo"                 "halo-pod@turing.ac.uk"            "Pod" 2)
+   (office-room "Logic"                "logic-pod@turing.ac.uk"           "Pod" 3)
+   (office-room "Nova"                 "nova-pod@turing.ac.uk"            "Pod" 3)
+   (office-room "Pixel"                "pixel-pod@turing.ac.uk"           "Pod" 1)
+   (office-room "Pulse"                "pulse-pod@turing.ac.uk"           "Pod" 1)
+   (office-room "Quark"                "quark-pod@turing.ac.uk"           "Pod" 1)
+   (office-room "Zen"                  "zen-pod@turing.ac.uk"             "Pod" 1)
+   
+   
+   ))
 
 
 
